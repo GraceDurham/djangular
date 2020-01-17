6df38b1be4a7af2f271d9c768a25bcf980c2f236
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2020-01-17 09:35:55 -0500

    add .gitignore file

---
+++ scrumboard/api.py
@@ -0,0 +1,15 @@
+from rest_framework.generics import ListAPIView
+
+
+from .serializers import ListSerializer, CardSerializer
+from .models import List, Card
+
+class ListApi(ListAPIView):
+	queryset = List.objects.all()
+	serializer_class = ListSerializer
+
+
+
+class CardApi(ListAPIView):
+	queryset = Card.objects.all()
+	serializer_class = CardSerializer
