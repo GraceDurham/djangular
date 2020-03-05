387207061683849aba3a2735cb55b1808a2a2a61
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2020-03-05 16:07:30 -0500

    add cardserializer to show cards in List API

--- scrumboard/serializers.py
+++ scrumboard/serializers.py
@@ -3,18 +3,20 @@ from rest_framework import serializers
 from .models import List, Card
 
 
-class ListSerializer(serializers.ModelSerializer):
-	
-	class Meta:
-		model = List
-		fields = '__all__'
-
-
 class CardSerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Card
 		fields = '__all__'
+
+
+class ListSerializer(serializers.ModelSerializer):
+	cards = CardSerializer(read_only=True, many=True)
+	
+	class Meta:
+		model = List
+		fields = '__all__'
+
 
 
 
