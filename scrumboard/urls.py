55b08dded709de243ad1c3cd0d7581351d7d11b4
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2020-03-09 09:33:56 -0400

    add router to ListViewSet CardViewSet map urls to these views

--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -1,11 +1,10 @@
-from django.conf.urls import url
+from .api import ListViewSet, CardViewSet
 
-from django.views.generic import TemplateView
+from rest_framework.routers import DefaultRouter
 
-from .api import ListApi, CardApi
+router = DefaultRouter()
+router.register(r'^lists$', ListViewSet)
+router.register(r'^cards$', CardViewSet)
 
-urlpatterns = [
-	url(r'^lists$', ListApi.as_view()),
-	url(r'^cards$', CardApi.as_view()),
-	url(r'^home', TemplateView.as_view(template_name="scrumboard/home.html")),
-]
+
+urlpatterns = router.urls
