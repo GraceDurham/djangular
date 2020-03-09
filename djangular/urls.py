24c8e90b7c368e4d4ddfee176ab12e27cd2324f2
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2020-03-09 09:40:18 -0400

    add home url to url.py djangular

--- djangular/urls.py
+++ djangular/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', include('scrumboard/home.html')),
     path('scrumboard/', include('scrumboard.urls')),
 ]
  
