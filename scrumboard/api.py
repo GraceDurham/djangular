64435c4eaac394a6b77233b37604b3a0ab913072
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2020-03-09 09:28:19 -0400

    add ModelViewSet to api.py save new cards with POST backend

--- scrumboard/api.py
+++ scrumboard/api.py
@@ -1,15 +1,17 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ModelViewSet
 
 
 from .serializers import ListSerializer, CardSerializer
 from .models import List, Card
 
-class ListApi(ListAPIView):
+class ListViewSet(ModelViewSet): # implements put post get delete methods on cards
 	queryset = List.objects.all()
 	serializer_class = ListSerializer
 
 
 
-class CardApi(ListAPIView):
+class CardViewSet(ModelViewSet):
 	queryset = Card.objects.all()
 	serializer_class = CardSerializer
+
+
