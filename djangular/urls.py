8f0a0fd62e34f26c699b01a1c1c8a9b8d56052be
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2020-03-25 13:58:40 -0400

    Update URLS and API views to Saving new cards with POST backend

--- djangular/urls.py
+++ djangular/urls.py
@@ -12,13 +12,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.conf.urls import url, include
-from django.urls import include, path
+from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', include('scrumboard/home.html')),
-    path('scrumboard/', include('scrumboard.urls')),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name="scrumboard/home.html")),
+    url(r'^scrumboard/', include('scrumboard.urls')),
 ]
  
--- scrumboard/urls.py
+++ scrumboard/urls.py
@@ -3,8 +3,8 @@ from .api import ListViewSet, CardViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'^lists$', ListViewSet)
-router.register(r'^cards$', CardViewSet)
+router.register(r'lists', ListViewSet)
+router.register(r'cards', CardViewSet)
 
 
 urlpatterns = router.urls
