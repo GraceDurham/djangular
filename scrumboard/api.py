426ccb82e6494b9d42eb5925448367cfa50479e3
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2020-03-30 13:37:25 -0400

    update js file to save new cards with POST front end

--- scrumboard/api.py
+++ scrumboard/api.py
@@ -4,14 +4,14 @@ from rest_framework.viewsets import ModelViewSet
 from .serializers import ListSerializer, CardSerializer
 from .models import List, Card
 
-class ListViewSet(ModelViewSet): # implements put post get delete methods on cards
+class ListViewSet(ModelViewSet): # ModelViewSet implements and supports put post get delete methods on cards
 	queryset = List.objects.all()
 	serializer_class = ListSerializer
 
 
 
 class CardViewSet(ModelViewSet):
-	queryset = Card.objects.all()
-	serializer_class = CardSerializer
+	queryset = List.objects.all()
+	serializer_class = ListSerializer
 
 
--- scrumboard/static/scrumboard/scrumboard.js
+++ scrumboard/static/scrumboard/scrumboard.js
@@ -2,18 +2,25 @@
 (function(){
 	'use strict';
 	angular.module('scrumboard.demo', [])
-		.controller('ScrumboardController', [ '$scope', '$http', ScrumboardController]);
+		.controller('ScrumboardController', ['$scope', '$http', ScrumboardController]);
 
 	function ScrumboardController($scope, $http){
 		$scope.add = function (list, title) {
 			var card = {
 				title: title
 			};
-			list.cards.push(card);
+			$http.post('/scrumboard/cards/', card)
+				.then(function(response){
+					list.cards.push(response.data); // response contains response from server 
+													// after saving card has database id that we can use it to create url that references id where we can use put delete method on card
+													// takes card object from server and pushes it onto list
+											   		// stores id of card to the server when user gets an error card	
+				});
+
 		};
 
 		$scope.data = []; 
-		$http.get('/scrumboard/lists').then(function(response){
+		$http.get('/scrumboard/lists/').then(function(response){
 			$scope.data = response.data;
 			//add $http.get data from url restframe work add then functions pass response from http call add scope.data to display JSON data
 		}); 
