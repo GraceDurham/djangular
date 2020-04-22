f98102a7a24bf29b23801c88681be7e1c8203851
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2020-04-22 13:21:22 -0400

    Add permission.IsAuthenticated classes to api.py to restrict views to users for CSRF Protection

--- scrumboard/api.py
+++ scrumboard/api.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
 
 
 from .serializers import ListSerializer, CardSerializer
@@ -7,9 +8,11 @@ from .models import List, Card
 class ListViewSet(ModelViewSet): # ModelViewSet implements and supports put post get delete methods on cards
 	queryset = List.objects.all()
 	serializer_class = ListSerializer
+	permission_classes = (permissions.IsAuthenticated,)
 
 
 
 class CardViewSet(ModelViewSet):
 	queryset = Card.objects.all()
 	serializer_class = CardSerializer
+	permission_classes = (permissions.IsAuthenticated,)
