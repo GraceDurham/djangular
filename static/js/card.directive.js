5470f3bb30da7b39d458ac2d383dbdcb18493344
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2021-02-05 17:32:26 -0800

    Move card.directive.js to js folder under main djangular folder

---
+++ static/js/card.directive.js
@@ -0,0 +1,46 @@
+(function () {
+	'use strict';
+
+	angular.module('scrumboard.demo')
+		.directive('scrumboardCard', CardDirective);
+
+
+	function CardDirective() {
+		return {
+			templateUrl: '/static/scrumboard/card.html',
+			restrict:'E',
+			controller: ['$scope', '$http', function ($scope, $http){
+				var url = '/scrumboard/cards/' + $scope.card.id + '/';
+				$scope.update = function () {
+					$http.put(
+						url,
+						$scope.card
+						
+					);
+				};
+
+				$scope.delete = function(){
+					$http.delete(url).then(
+						function(){
+							var cards = $scope.list.cards;
+							cards.splice(
+								cards.indexOf($scope.card),
+								1
+							);
+						}
+
+					);
+
+				};
+
+				$scope.modelOptions = {
+					debounce:500 //Add model options Debounce when event causes model 
+								 //to be updated waits for 500 milliseconds to update change
+				};
+			}]
+
+		};
+
+	}
+
+})();
