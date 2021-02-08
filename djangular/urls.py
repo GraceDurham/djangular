977fa8b94752d11a91c1ea06791059fba7db89e7
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2021-02-08 11:58:09 -0800

    update home url in urls.py under djangular app since no longer under scrumboard app

--- djangular/urls.py
+++ djangular/urls.py
@@ -20,8 +20,8 @@ from django.views.decorators.csrf import ensure_csrf_cookie
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', ensure_csrf_cookie(TemplateView.as_view(template_name = "scrumboard/home.html"))), # add csrf cookie decorator for home view 
-    url(r'^$', TemplateView.as_view(template_name="scrumboard/home.html")),
+    url(r'^$', ensure_csrf_cookie(TemplateView.as_view(template_name = "home.html"))), # add csrf cookie decorator for home view 
+    # url(r'^$', TemplateView.as_view(template_name="scrumboard/home.html")),
     url(r'^scrumboard/', include('scrumboard.urls')),
     url(r'^auth_api/', include('auth_api.urls')),
 ]
