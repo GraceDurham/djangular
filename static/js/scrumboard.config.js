c67ba892ce8dffc2d5c7f368103103d862ebcc74
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2021-02-08 16:39:59 -0800

    add config function to scrumboard.config.js to configure urls and views of login and '/'

--- static/js/scrumboard.config.js
+++ static/js/scrumboard.config.js
@@ -1,7 +1,24 @@
 (function () {
 	'use strict';
 
-	angular.module('scrumboard.demo').run(['$http', run]);
+	angular.module('scrumboard.demo')
+		.config(['$routeProvider', config])
+		.run(['$http', run]);
+
+
+	function config($routeProvider) {
+
+		$routeProvider
+			.when('/', {
+				templateUrl: '/static/html/scrumboard.html',
+				controller: 'ScrumboardController',
+			})
+			.when('/login', {
+				templateUrl: '/static/html/login.html'
+
+			})
+			.otherwise('/');
+	}
 
 	function run($http) {
 		$http.defaults.xsrfHeaderName = 'X-CSRFToken';
