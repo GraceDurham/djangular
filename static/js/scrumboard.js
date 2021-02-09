13fdf5d411d620419bc019794f1a15885a7947c7
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2021-02-08 16:26:18 -0800

    add ngRoute module as a dependency in scrumboard.js

--- static/js/scrumboard.js
+++ static/js/scrumboard.js
@@ -1,7 +1,7 @@
 
 (function(){
 	'use strict';
-	angular.module('scrumboard.demo', [])
+	angular.module('scrumboard.demo', ['ngRoute'])
 		.controller('ScrumboardController', ['$scope', '$http', ScrumboardController]);
 
 	function ScrumboardController($scope, $http){
