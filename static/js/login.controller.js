ffd522eb1999d2cee463b51b72dd2c79529fc721
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2021-02-09 12:21:41 -0800

    add login.controller.js

---
+++ static/js/login.controller.js
@@ -0,0 +1,22 @@
+(function (){
+	'use strict';
+
+
+	angular
+		.module('scrumboard.demo')
+		.controller('LoginController', ['$scope', '$http', '$location', LoginController]);
+
+	function LoginController($scope, $http, $location){
+		$scope.login =function() {
+			$http.post('/auth_api/login/', $scope.user)
+				.then(function (){
+					$location.url('/');
+				},
+				function(){
+					$scope.login_error="Invalid username/password combination";
+				});
+
+
+		}
+	}
+}) ();
